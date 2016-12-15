cc57ee41a82782773181e0b5085cc851d448dcc4
Rolando Albarnez <rolando.albarnez@sansano.usm.cl>
2016-12-15 13:11:35 -0800

    Update MultiThreads1.py

--- MultiThreads1.py
+++ MultiThreads1.py
@@ -1,3 +1,20 @@
-import os, sys, thread
+import os, sys, thread, time
+import subprocess
 
-def 
+def time():
+    timeN = time.strftime("%H:%M:%S").split(':')
+    return timeN
+
+def main():
+    #print '##########################: ',thread.start_new_thread(imprimir_mensaje, (mensaje,))
+        #print '#######################################################################'
+        #print '           CUANTOS PROCESOS(PROGRAMAS) CORRERAS SIMULTANEAMENTE?       '
+        #print '#######################################################################'
+        #choise1 = int(raw_input('INGRESE UN ENTERO: '))
+        
+    thread.start_new_thread(procesos, (treadNow,)
+    thread.start_new_thread(opt, (treadNow1,))
+
+    
+    
+main()
